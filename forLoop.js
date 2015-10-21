2f88535154984e07dc3af5e57eed2937c9d3ed3d
Vuk Dukic <VukDukic@users.noreply.github.com>
2015-10-21 13:27:07 -0700

    Create forLoop.js
    The loop initialization where we initialize our counter to a starting
    value. The initialization statement is executed before the loop begins.
     The test statement which will test if a given condition is true or not. If
    the condition is true, then the code given inside the loop will be executed,
    otherwise the control will come out of the loop.
     The iteration statement where you can increase or decrease your
    counter.

---
+++ forLoop.js
@@ -0,0 +1,20 @@
+for (initialization; test condition; iteration statement){
+Statement(s) to be executed if test condition is true
+}
+
+<html>
+<body>
+<script type="text/javascript">
+<!--
+var count;
+document.write("Starting Loop" + "<br />");
+for(count = 0; count < 10; count++){
+document.write("Current Count : " + count );
+document.write("<br />");
+}
+document.write("Loop stopped!");
+//-->
+</script>
+<p>Set the variable to different value and then try...</p>
+</body>
+</html>
