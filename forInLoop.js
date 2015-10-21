660f6a2ac80b6a25f45119b051320ea2a39275ab
Vuk Dukic <VukDukic@users.noreply.github.com>
2015-10-21 13:31:11 -0700

    Create forInLoop.js
    for...in loop is used to loop through an object's properties. As we have not
    discussed Objects yet, you may not feel comfortable with this loop. But once you
    understand how objects behave in JavaScript, you will find this loop very useful.

---
+++ forInLoop.js
@@ -0,0 +1,18 @@
+for (variablename in object){
+statement or block to execute
+}
+
+<html>
+<body>
+<script type="text/javascript">
+<!--
+var aProperty;
+document.write("Navigator Object Properties<br /> ");
+for (aProperty in navigator)
+{
+document.write(aProperty);
+document.write("<br />");
+}
+document.write ("Exiting from the loop!");
+//-->
+</script>
