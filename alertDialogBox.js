36d3d2e7177e82d69525c1fb7a683747a0963eda
Vuk Dukic <VukDukic@users.noreply.github.com>
2015-10-29 22:48:09 -0700

    Create alertDialogBox.js

---
+++ alertDialogBox.js
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <script type="text/javascript">
+    <!--
+    function Warn() {
+      alert ("This is a warning message!");
+      document.write("This is a warning message!");
+    }
+    //-->
+    </script>
+  </head>
+  <body>
+    <p>Click the following to see the result: </p>
+      <form>
+        <input type="button" value="Click Me" onclick+"Warn();"/>
+      </form>
+    </body>
+  </html>
+  
+  
